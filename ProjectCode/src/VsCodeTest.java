9787e2c6c81e0e02bb538876cb2af395c57d8b4b
John <dirksej@mail.gvsu.edu>
2021-10-11 16:13:51 -0400

    added new class from vs code

---
+++ ProjectCode/src/VsCodeTest.java
@@ -0,0 +1,16 @@
+
+
+public class VsCodeTest {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 5;
+
+        for (int i = 0; i < 5; i++){
+            a += 5;
+        }
+
+        System.out.println(a);
+        System.out.println(b + " hello world!");
+
+    }
+}
