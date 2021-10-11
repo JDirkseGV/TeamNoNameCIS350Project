613e2bbcd24a878836dfe67502f50c277b8c1456
Dakota Sliter <djsliter@gmail.com>
2021-10-10 23:51:47 -0400

    Added "And Hello from Dakota!" as a test

--- ProjectCode/src/FirstFileTest.java
+++ ProjectCode/src/FirstFileTest.java
@@ -1,6 +1,7 @@
 public class FirstFileTest {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("And Hello from Dakota!")
 
     }
 }
