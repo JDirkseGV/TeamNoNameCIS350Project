b953ff18ee5af8cfa3f5f25f04b20a636a1bae5b
Dakota Sliter <djsliter@gmail.com>
2021-10-11 00:22:25 -0400

    Environment setup test for Dakota

--- ProjectCode/src/FirstFileTest.java
+++ ProjectCode/src/FirstFileTest.java
@@ -1,7 +1,7 @@
 public class FirstFileTest {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("And Hello from Dakota!")
+        System.out.println("And Hello from Dakota!");
 
     }
 }
