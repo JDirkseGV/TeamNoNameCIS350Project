c653cad8ad61c29722b0f93640681a1c3d389591
John <dirksej@mail.gvsu.edu>
2021-10-11 16:25:23 -0400

    added a person object class to store info about a person and called it in main

--- ProjectCode/src/VsCodeTest.java
+++ ProjectCode/src/VsCodeTest.java
@@ -12,5 +12,11 @@ public class VsCodeTest {
         System.out.println(a);
         System.out.println(b + " hello world!");
 
+        Person human = new Person(20, "John");
+
+        System.out.println(human.toString());
+        System.out.println(human.getName() + " is ");
+        System.out.println(human.getAge() + " years old");
+
     }
 }
